e05db8842e36a85e065d1a6782a078a301c08d92
victhor409 <victhor409@gmail.com>
2020-02-03 17:14:16 -0300

    DEMO MAP 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,29 @@
 package application;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+
+import entites.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
+
 		
-		//map declarado, vamos trabalhar com cookies 
-		Map<String, String> cookies = new TreeMap<>();
+		Map<Product, Double> stock = new HashMap<>();
+
+		Product p1 = new Product("Tv", 900.0);
+		Product p2 = new Product("Notebook", 1200.0);
+		Product p3 = new Product("Tablet", 400.0);
 		
-		cookies.put("username","Maria");
-		cookies.put("email", "maria@gmail.com");
-		cookies.put("phone", "99711122");
+		stock.put(p1, 10000.0);
+		stock.put(p2, 20000.0);
+		stock.put(p3, 15000.0);
 		
-		cookies.remove("email");
-		cookies.put("phone", "99771133");
+		Product ps = new Product("Tv", 900.0);
 		
-		System.out.println("Contains 'phone' key: "+ cookies.containsKey("phone"));
-		
-		System.out.println("ALL COOKIES: ");
-		for (String key : cookies.keySet()) {
-			System.out.println(key+": "+ cookies.get(key));
-		}
-		
+		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
 
 	}
 
